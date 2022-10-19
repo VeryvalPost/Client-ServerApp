ca3ea047cbb051904fe3d279244370981d93b0f7
VeryvalPost <112349610+VeryvalPost@users.noreply.github.com>
2022-10-19 23:02:15 +0300

    Update Server.java
    changes

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -9,25 +9,32 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8083); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
-        Socket clientSocket = serverSocket.accept(); // ждем подключения
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+       int port = 8083;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept(); // ждем подключения
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ) {
+                    System.out.println("New connection accepted" + "\n" + "Порт подключения:" + clientSocket.getPort());
+                    String name = in.readLine();
+                    out.println(String.format("Hi %s, your port is %d. Are you child? (yes/no)",
+                            name, clientSocket.getPort()));
+                    String answer = in.readLine();
 
+                    if (answer.equals("yes")) {
+                        out.println("Welcome to the kids area, " + name + "! Let's play!");
+                    } else if (answer.equals("no")) {
+                        out.println("Welcome to the adult zone, " + name + "! Have a good rest, or a good working day!");
+                    }
+                    String quit = in.readLine();
+                    System.out.println(quit);
+                    out.println(quit);
 
-        System.out.println("New connection accepted" + "\n" + "Порт подключения:" + clientSocket.getPort());
-        String name = in.readLine();
-        out.println(String.format("Hi %s, your port is %d. Are you child? (yes/no)",
-                name, clientSocket.getPort()));
-        String answer = in.readLine();
-
-        if (answer.equals("yes")) {
-            out.println("Welcome to the kids area, " + name + "! Let's play!");
-        } else if (answer.equals("no")) {
-            out.println("Welcome to the adult zone, " + name + "! Have a good rest, or a good working day!");
+                } catch (IOException ex) {
+                    ex.printStackTrace(System.out);
+                }
+            }
         }
-        String quit = in.readLine();
-        System.out.println(quit);
-        out.println(quit);
     }
 }
